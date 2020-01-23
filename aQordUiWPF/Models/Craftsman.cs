102f91f6b05d4b2cd8c6a913f7ed7f3839071a30
Qu <Quanvle@live.dk>
2020-01-23 20:32:40 +0100

    add Craftsman class model to WPF project

---
+++ aQordUiWPF/Models/Craftsman.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace aQordUiWPF
+{
+    public class Craftsman
+    {
+        public int Id { get; set; }
+        public string Profession { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal HourlyRate { get; set; }
+        public double WorkingHourDaily { get; set; }
+        public double WorkingHourWeekly { get; set; }
+
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Id},{Profession},{FirstName},{LastName},{HourlyRate},{WorkingHourDaily},{WorkingHourWeekly}");
+        }
+    }
+}
